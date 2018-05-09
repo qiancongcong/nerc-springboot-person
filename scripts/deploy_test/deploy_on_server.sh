d4cf1e560a88e110204f5ba23c219d70f21b58d2
qiancongcong <38574286+qiancongcong@users.noreply.github.com>
2018-05-09 18:21:35 +0800

    Update deploy_on_server.sh

--- scripts/deploy_test/deploy_on_server.sh
+++ scripts/deploy_test/deploy_on_server.sh
@@ -3,5 +3,5 @@ export JAVA_HOME PATH CLASSPATH
 JAVA_HOME=/usr/java/jdk1.8.0_161
 PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
 CLASSPATH=.:$JAVA_HOME/lib:$JAVA_HOME/jre/lib:$CLASSPATH
-nohup java -jar /soft/zhihu/com.nerc.zhihu-0.0.1-SNAPSHOT.jar &
+nohup java -jar com.nerc.zhihu-0.0.1-SNAPSHOT.jar &
 exit
